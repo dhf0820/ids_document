55c941af1c0fee1f340d5f53182ae7ec4a091e66
Don French <dhfrench@vertisoft.com>
2022-05-15 16:07:22 -0700

    remove old _service name

--- go.mod
+++ go.mod
@@ -6,9 +6,11 @@ go 1.17
 
 require (
 	github.com/davecgh/go-spew v1.1.1
+	github.com/dhf0820/ids_model v0.0.0-20220515172558-be5cedce2f8f
 	github.com/golang/protobuf v1.5.2
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.4.0
+	github.com/pdfcpu/pdfcpu v0.3.13
 	github.com/sirupsen/logrus v1.8.1
 	github.com/smartystreets/goconvey v1.7.2
 	go.mongodb.org/mongo-driver v1.8.4
@@ -20,6 +22,8 @@ require (
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/gopherjs/gopherjs v0.0.0-20181017120253-0766667cb4d1 // indirect
+	github.com/hhrutter/lzw v0.0.0-20190829144645-6f07a24e8650 // indirect
+	github.com/hhrutter/tiff v0.0.0-20190829141212-736cae8d0bc7 // indirect
 	github.com/jtolds/gls v4.20.0+incompatible // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -29,9 +33,11 @@ require (
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
+	golang.org/x/image v0.0.0-20210220032944-ac19c3e999fb // indirect
 	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
-	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/text v0.3.6 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- services/deliver.go
+++ services/deliver.go
@@ -1,116 +1,116 @@
 package services
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	//"github.com/davecgh/go-spew/spew"
-	"github.com/dhf0820/ids_model/common"
-	docMod "github.com/dhf0820/ids_model/document"
-	"github.com/dhf0820/ids_model/logging"
+// 	//"github.com/davecgh/go-spew/spew"
+// 	"github.com/dhf0820/ids_model/common"
+// 	docMod "github.com/dhf0820/ids_model/document"
+// 	"github.com/dhf0820/ids_model/logging"
 
-	//"io/ioutil"
-	//"log"
-	//"os"
-	"strconv"
-	"strings"
-	"time"
-	//"gopkg.in/gomail.v2"
-)
+// 	//"io/ioutil"
+// 	//"log"
+// 	//"os"
+// 	"strconv"
+// 	"strings"
+// 	"time"
+// 	//"gopkg.in/gomail.v2"
+// )
 
 //Delivery: accepts the Payload and the slice of delivery documents and Dellivers them how required
 /// DownloadConnector takes the dDocs list of document files and combines them in ot one image saving it in the image repository
 
-func Deliver(payload *common.Payload, dDocs []*docMod.DeliveryDocument) error {
-	var err error
-	inFiles := []string{}
-	for _, doc := range dDocs {
-		inFiles = append(inFiles, doc.FileName)
-	}
+// func Deliver(payload *common.Payload, dDocs []*docMod.DeliveryDocument) error {
+// 	var err error
+// 	inFiles := []string{}
+// 	for _, doc := range dDocs {
+// 		inFiles = append(inFiles, doc.FileName)
+// 	}
 
-	// url, image, err := GetCombinedRelease(payload.DelvPayload.ReleaseID)
-	// if err != nil {  // Combined Image does not exist
-	// 	// Save the combined image  to the document repository and update release adding the image url
-	// 	MergeName := fmt.Sprintf("rel-%s.pdf", payload.DelvPayload.ReleaseID.Hex())
-	// 	err = MergeToCombined(inFiles, MergeName, false)
-	// 	if err != nil {
-	// 		err = fmt.Errorf("MergeToCombine: %s", err.Error())
-	// 		return err
-	// 	}
-	// 	url := SaveCombinedRelease(MergeName)
-	// 	image, err =
-	// }
-	// Combined release iomage is stores and accessable via url
-	// That should be all that is required for download delivery. The remote will now request the
+// 	// url, image, err := GetCombinedRelease(payload.DelvPayload.ReleaseID)
+// 	// if err != nil {  // Combined Image does not exist
+// 	// 	// Save the combined image  to the document repository and update release adding the image url
+// 	// 	MergeName := fmt.Sprintf("rel-%s.pdf", payload.DelvPayload.ReleaseID.Hex())
+// 	// 	err = MergeToCombined(inFiles, MergeName, false)
+// 	// 	if err != nil {
+// 	// 		err = fmt.Errorf("MergeToCombine: %s", err.Error())
+// 	// 		return err
+// 	// 	}
+// 	// 	url := SaveCombinedRelease(MergeName)
+// 	// 	image, err =
+// 	// }
+// 	// Combined release iomage is stores and accessable via url
+// 	// That should be all that is required for download delivery. The remote will now request the
 
-	smtpServer := payload.Config.ConnectAddress.Address
-	auth := payload.Config.ConnectAddress.Authorization
-	userName := ""
-	password := ""
-	if auth != "" {
-		auths := strings.Split(auth, ":")
-		//fmt.Printf("Auth: %s\n", auths)
-		userName = auths[0]
-		password = auths[1]
-	}
-	fmt.Printf("Access:%s:%s\n", userName, password)
-	fromFld, err := common.GetFieldByName(payload.Config.Fields, "from")
-	if err != nil {
-		fmt.Printf("From Error: %s\n", err.Error())
-	}
-	toFld, err := common.GetFieldByName(payload.Device.Fields, "to")
-	if err != nil {
-		fmt.Printf("toFld Error: %s\n", err.Error())
-	}
-	fmt.Printf("Sending from: %s  to: %s\n", fromFld.Default, toFld.Value)
-	gm := gomail.NewMessage()
-	gm.SetHeader("From", fromFld.Default)
-	gm.SetHeader("To", toFld.Value)
-	fmt.Printf("---Sending to: %s---\n", toFld.Value)
-	gm.SetHeader("Subject", "#secure# Requested Secure Medical Records")
+// 	smtpServer := payload.Config.ConnectAddress.Address
+// 	auth := payload.Config.ConnectAddress.Authorization
+// 	userName := ""
+// 	password := ""
+// 	if auth != "" {
+// 		auths := strings.Split(auth, ":")
+// 		//fmt.Printf("Auth: %s\n", auths)
+// 		userName = auths[0]
+// 		password = auths[1]
+// 	}
+// 	fmt.Printf("Access:%s:%s\n", userName, password)
+// 	fromFld, err := common.GetFieldByName(payload.Config.Fields, "from")
+// 	if err != nil {
+// 		fmt.Printf("From Error: %s\n", err.Error())
+// 	}
+// 	toFld, err := common.GetFieldByName(payload.Device.Fields, "to")
+// 	if err != nil {
+// 		fmt.Printf("toFld Error: %s\n", err.Error())
+// 	}
+// 	fmt.Printf("Sending from: %s  to: %s\n", fromFld.Default, toFld.Value)
+// 	gm := gomail.NewMessage()
+// 	gm.SetHeader("From", fromFld.Default)
+// 	gm.SetHeader("To", toFld.Value)
+// 	fmt.Printf("---Sending to: %s---\n", toFld.Value)
+// 	gm.SetHeader("Subject", "#secure# Requested Secure Medical Records")
 
-	meta := payload.DelvPayload.Meta
-	dob := common.GetDataByName(meta, "dob")
-	patientName := common.GetDataByName(meta, "patient_name")
+// 	meta := payload.DelvPayload.Meta
+// 	dob := common.GetDataByName(meta, "dob")
+// 	patientName := common.GetDataByName(meta, "patient_name")
 
-	body := fmt.Sprintf("Requested Medical Records for Patient: <b>%s</b> -dob: <i>%s</i>!", patientName, dob)
-	gm.SetBody("text/html", body)
-	for _, dDoc := range dDocs {
+// 	body := fmt.Sprintf("Requested Medical Records for Patient: <b>%s</b> -dob: <i>%s</i>!", patientName, dob)
+// 	gm.SetBody("text/html", body)
+// 	for _, dDoc := range dDocs {
 
-		fmt.Printf("     Attaching file: [%s]  name: [%s]\n", dDoc.Description, dDoc.FileName)
-		//fmt.Printf("attached Delivery: %s\n", spew.Sdump(dDoc))
-		//fi, _ := os.Stat(dDoc.FileName)
-		//fmt.Printf("Size of %s : %d\n\n", dDoc.FileName, fi.Size())
-		gm.Attach(dDoc.FileName)
-	}
-	fmt.Printf("SMTP SERVER: %s\n", smtpServer)
-	var port int
-	hostInfo := strings.Split(smtpServer, ":")
-	if len(hostInfo) > 1 {
-		port, err = strconv.Atoi(hostInfo[1])
-		if err != nil {
-			port = 587
-		}
-	}
-	smtpServer = hostInfo[0]
-	d := gomail.NewDialer(smtpServer, port, userName, password)
-	startTime := time.Now()
-	if err := d.DialAndSend(gm); err != nil {
-		_ = removeTempFile(dDocs)
-		fmt.Printf("DialAndSend error: %v\n", err)
-		return err
-	}
-	msg := fmt.Sprintf("Send email of %d documents took %f seconds", len(dDocs), time.Since(startTime).Seconds)
-	logging.LogMessage(payload, "logs", "success", msg, "email_connector")
-	return nil
-}
+// 		fmt.Printf("     Attaching file: [%s]  name: [%s]\n", dDoc.Description, dDoc.FileName)
+// 		//fmt.Printf("attached Delivery: %s\n", spew.Sdump(dDoc))
+// 		//fi, _ := os.Stat(dDoc.FileName)
+// 		//fmt.Printf("Size of %s : %d\n\n", dDoc.FileName, fi.Size())
+// 		gm.Attach(dDoc.FileName)
+// 	}
+// 	fmt.Printf("SMTP SERVER: %s\n", smtpServer)
+// 	var port int
+// 	hostInfo := strings.Split(smtpServer, ":")
+// 	if len(hostInfo) > 1 {
+// 		port, err = strconv.Atoi(hostInfo[1])
+// 		if err != nil {
+// 			port = 587
+// 		}
+// 	}
+// 	smtpServer = hostInfo[0]
+// 	d := gomail.NewDialer(smtpServer, port, userName, password)
+// 	startTime := time.Now()
+// 	if err := d.DialAndSend(gm); err != nil {
+// 		_ = removeTempFile(dDocs)
+// 		fmt.Printf("DialAndSend error: %v\n", err)
+// 		return err
+// 	}
+// 	msg := fmt.Sprintf("Send email of %d documents took %f seconds", len(dDocs), time.Since(startTime).Seconds)
+// 	logging.LogMessage(payload, "logs", "success", msg, "email_connector")
+// 	return nil
+// }
 
-func removeTempFile(dDocs []*docMod.DeliveryDocument) error {
-	for _, dDoc := range dDocs {
-		fmt.Printf("Remove %s\n", dDoc.FileName)
-		//os.Remove(dDoc.FileName)
-		//if err != nil {
-		//	fmt.Printf("Error removing tempFile %s: %v\n", doc.FileName, err)
-		//}
-	}
-	return nil
-}
+// func removeTempFile(dDocs []*docMod.DeliveryDocument) error {
+// 	for _, dDoc := range dDocs {
+// 		fmt.Printf("Remove %s\n", dDoc.FileName)
+// 		//os.Remove(dDoc.FileName)
+// 		//if err != nil {
+// 		//	fmt.Printf("Error removing tempFile %s: %v\n", doc.FileName, err)
+// 		//}
+// 	}
+// 	return nil
+// }
